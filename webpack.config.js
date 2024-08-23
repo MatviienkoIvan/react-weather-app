e37af129820da80bdf4371b034571166f1206a36
Matviienko.Ivan <matviienko.i.o@gmail.com>
2024-08-23 13:51:01 +0200

    SCSS added

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
             WeatherMessage: 'app/components/WeatherMessage.jsx',
             openWeatherAPI: 'app/api/openWeatherAPI.jsx',
             ErrorModal: 'app/components/ErrorModal.jsx',
-            applicationStyles: 'app/styles/app.css'
+            applicationStyles: 'app/styles/app.scss'
         },
         extensions: ['', '.js', '.jsx']
     },
