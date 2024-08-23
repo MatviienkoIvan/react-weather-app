f00dbcb4d3eea812c931d0b757a17cbda89ea16e
Matviienko.Ivan <matviienko.i.o@gmail.com>
2024-08-23 11:47:23 +0200

    Add about page styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,21 @@
 var React = require("react");
 
 var About = (props) => {
-  return <h2>About Component</h2>;
+  return (
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a wather app build on React.</p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the javascript library used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used it to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 module.exports = About;
