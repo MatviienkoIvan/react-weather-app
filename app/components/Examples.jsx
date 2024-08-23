6dc47160e4a1ea5fd43816780a3d83b40e6222f7
Matviienko.Ivan <matviienko.i.o@gmail.com>
2024-08-23 11:41:01 +0200

    Add examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,21 @@
 var React = require("react");
+var {Link} = require('react-router');
 
 var Examples = (props) => {
-  return <h2>Examples Component</h2>;
+  return (
+    <div>
+      <h1 className="text-center">Example</h1>
+      <p>Here're a few example locations to try out:</p>    
+      <ol>
+        <li>
+          <Link to='/?location=Opole'>Opole, OPO</Link>
+        </li>
+        <li>
+          <Link to='/?location=Warszaw'>Warszaw</Link>
+        </li>
+      </ol>
+    </div>
+  );
 };
 
 module.exports = Examples;
